0a60425cb6c3affc37f48c81bfd92695fd046e6a
Ahmed Alfadhel <107873451+AhmedAlfadhel22@users.noreply.github.com>
2022-08-16 10:39:19 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+print("Welcome to Password Generator!")
+name = input("Name: ")
+length = input("Length of Password: ")
+
+if length == "1":
+    input("Length is inappropiate! Try Again: ")
+
+if length == "2":
+    input("Length is inappropiate! Try Again: ")
+
+if length == "3":
+    input("Length is inappropiate! Try Again: ")
+
+if length == "4":
+    input("Length is inappropiate! Try Again: ")
+
+if length == "5":
+    input("Length is inappropiate! Try Again: ")
+
+if length == "6":
+    input("Length is inappropiate! Try Again: ")
+
+if length == "7":
+    input("Length is inappropiate! Try Again: ")
+
+if length == "8":
+    print("opsdjd8>")
+
+if length == "9":
+    print("AHDjsnm6:")
+
+if length == "10":
+    print("ZxcVCvb98@")
+
+print("Thank you " + name + " For trying Password Generator")
